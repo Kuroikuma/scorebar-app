0ba921e6f6c84da70ffabe66cee7a411f7b53676
Kuroikuma <odiseoj676@gmail.com>
2025-01-07 00:52:54 -0600

    Refactor socket connection to remove withCredentials option and add CORS headers

--- app/service/socket.ts
+++ app/service/socket.ts
@@ -1,7 +1,6 @@
 import io from 'socket.io-client'
 
 const  socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:5001', {
-  withCredentials: true,
   extraHeaders: {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
