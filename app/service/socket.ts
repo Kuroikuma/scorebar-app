2cc461ba93347e46c5735b53c7e82d5960c486e5
Kuroikuma <odiseoj676@gmail.com>
2025-01-06 23:41:13 -0600

    Refactor socket connection to use NEXT_PUBLIC_SOCKET_URL environment variable

--- app/service/socket.ts
+++ app/service/socket.ts
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
 
-const  socket = io(process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001')
+const  socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:5001')
 
 export default socket
